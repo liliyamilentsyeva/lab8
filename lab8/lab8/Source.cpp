61e11ea3c0492d95f558ee51443fa36ae72825ed
lilya9696 <lilya9696@mail.ru>
2014-04-18 12:14:46 -0700

    Implemented lab8

---
+++ lab8/lab8/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cin>>a>>b;
+	_asm
+	{
+		mov eax, a
+		mov ebx, b
+		add eax, ebx
+		mov c, eax
+	}
+	cout<<c<<endl;
+	return 0;
+}
+	
